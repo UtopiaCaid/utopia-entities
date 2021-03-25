7e9e163bb07dc785ed1664b77baa8a2072ac4896
Chris Quintanilla <christopher.quintanilla@smoothstack.com>
2021-03-24 22:44:51 -0400

    Bug: Removed non entity java classes, removed dependecies to prepare for usage as a module

--- src/main/java/com/caid/utopia/UtopiaApplication.java
+++ src/main/java/com/caid/utopia/UtopiaApplication.java
@@ -1,13 +1,13 @@
-package com.caid.utopia;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class UtopiaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UtopiaApplication.class, args);
-	}
-
-}
+//package com.caid.utopia;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class UtopiaApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(UtopiaApplication.class, args);
+//	}
+//
+//}
